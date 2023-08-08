0a5f73e61bd41d0180e00c7bf2c3650658c2730a
zachlamont <zachlamont1@gmail.com>
2023-08-08 21:05:20 +1000

    Complete Part 2 of the tutorial

---
+++ express-locallibrary-tutorial/routes/users.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET users listing. */
+//http://localhost:3000/users
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+
+//http://localhost:3000/users/cool
+router.get("/cool", function (req, res, next) {
+  res.send("You're so cool");
+});
+
+module.exports = router;
