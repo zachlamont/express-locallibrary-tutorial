cfd51d464ec7ce0b0a1090963747c2669fd15804
zachlamont <zachlamont1@gmail.com>
2023-08-15 20:34:21 +1000

    Add rate limiter

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ const helmet = require("helmet");
 
 const app = express();
 
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+
 // Add helmet to the middleware chain.
 // Set CSP headers to allow our Bootstrap and Jquery to be served
 app.use(
