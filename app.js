cb03ba0606dc409338f214ba8baf8afc83608f57
zachlamont <zachlamont1@gmail.com>
2023-08-15 20:31:01 +1000

    Add helmet

--- app.js
+++ app.js
@@ -11,8 +11,19 @@ const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog"); //Import routes for 'catalog' area of site
 
 const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -33,6 +44,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
